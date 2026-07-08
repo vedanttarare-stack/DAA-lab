ba533e129af41487ff25f91da6f9616b74ce7a82
vedanttarare-stack <vedanttarare@gmail.com>
2026-07-08 14:00:56 +0530

    Create exp1.1.c

---
+++ exp_1.1.1/exp1.1.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+// Quick Sort function
+void quickSort(int arr[], int low, int high) {
+    // Complete the code...
+        if (low < high) {
+        int pi = partition(arr, low, high);
+
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+	
+
+
+    
+    
+}
+
+// Function to print an array
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int n;
+    // Input the size of the array
+    scanf("%d", &n);
+    int arr[n];
+    
+    // Input the elements of the array
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Print the original array
+    printArray(arr, n);
+
+    // Sort the array using quick sort
+    quickSort(arr, 0, n - 1);
+    
+    // Print the sorted array
+    printArray(arr, n);
+
+    return 0;
+}
